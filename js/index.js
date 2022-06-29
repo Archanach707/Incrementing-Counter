e9c277cf45e6b6e909b415119eb7c30ba7491be0
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2022-06-29 16:02:06 +0530

    1.modification on html file, 2. add propery in css file, 3. js function appyed of incrementing counter

--- js/index.js
+++ js/index.js
@@ -4,6 +4,19 @@ counters.forEach(counter => {
   counter.innerText = '0'
 
   const updateCounter = () => {
-    const target = counters.getAttribute('data-arget')
+    const target = +counter.getAttribute('data-target')
+    const c = +counter.innerText
+
+    const increment = target / 1000 
+
+    if(c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}`
+      setTimeout(updateCounter, 1)
+
+    }else {
+      counter.innerText = target
+    }
   }
+
+  updateCounter()
 })
